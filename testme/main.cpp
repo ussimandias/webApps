66128cfa666715eb23390c4ea6f96ff9964ecc23
uss <utarfa@gmail.com>
2013-10-04 21:30:35 -0400

    simple math in c++

--- testme/main.cpp
+++ testme/main.cpp
@@ -8,11 +8,26 @@
 
 #include <iostream>
 
+
 int main(int argc, const char * argv[])
 {
 
     // insert code here...
     std::cout << "Hello, World!\n";
+    
+    std::cout << "pls enter a two numbers: "<< std::endl;
+    
+    int i;
+    int j;
+    
+    std::cin >> i;
+    std::cin >> j;
+    
+    std::cout << "the number you entered is:"<<i << " and " << j << std::endl;
+    std::cout << "the sum of " << i << " and "<< j << " is " << i + j << std::endl;
+    
+    
+    
     return 0;
 }
 
